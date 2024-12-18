c2c6c84cbebdb23a85d3cd5cda9885b60f77800d
John Ericksen <john.paul.ericksen@gmail.com>
2024-12-18 15:57:46 -0500

    bundle as executable

---
+++ Newsletter Scraper/README.md
@@ -0,0 +1,14 @@
+# Newsletter Scraper
+
+This script looks through the downloaded PDF version of the CAS newsletter, and
+pulls out all the data that is bolded for storage in the newsletter spreadsheet.
+
+## Instructions
+
+1. Copy the file path of your downloaded PDF version of the newsletter you want to scan.
+2. Click on the scraper executable.
+3. The program will display `exit;` before running. Please be patient as it may take some to start up.
+4. Paste in the newsletter file path when prompted.
+5. Once completed, the program will return the bolded text from the PDF, which can be used to fill out the newsletter content spreadsheet.
+
+Hope this is helpful :)
--- scraper.py
+++ scraper.py
@@ -1,22 +1,28 @@
 import pdfplumber
 
-
 def createPDF():
     try:
         pdfPath = input("Enter the path of the Newsletter PDF file: ")
         pdf = pdfplumber.open(pdfPath)
         return pdf
-    except FileNotFoundError:
+    except FileNotFoundError or TypeError:
         print("The file was not found; your path is most likely incorrect! Please try again.")
         createPDF()
 
 pdf = createPDF()
+
+print("\n")
+print("-----------------------------")
+print("\n")
+
 for page in pdf.pages:
     text = page
     clean_text = text.filter(lambda obj: obj["object_type"] == "char" and "Bold" in obj["fontname"])
     print(clean_text.extract_text())
 
-
+print("\n")
+print("-----------------------------")
+print("\n")
 
 
 
